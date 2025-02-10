49662284156a1eb46a78889f3d258fda746525e7
Mostafa Morsy <mostafa.morsy@intalio.com>
2025-02-10 16:19:07 +0200

    Initial Commit

---
+++ crazyCryptoSwift/crazyCryptoSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  crazyCryptoSwiftApp.swift
+//  crazyCryptoSwift
+//
+//  Created by mostafa Morsy on 10/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct crazyCryptoSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ crazyCryptoSwiftTests/crazyCryptoSwiftTests.swift
@@ -0,0 +1,17 @@
+//
+//  crazyCryptoSwiftTests.swift
+//  crazyCryptoSwiftTests
+//
+//  Created by mostafa Morsy on 10/02/2025.
+//
+
+import Testing
+@testable import crazyCryptoSwift
+
+struct crazyCryptoSwiftTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
